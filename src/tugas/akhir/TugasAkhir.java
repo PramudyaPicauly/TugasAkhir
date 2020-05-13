0185cd21e906761d4e7bcffe1be0f48518ed507a
Pramudya Anggara <pramudyaa2001@gmail.com>
2020-05-13 20:18:21 +0700

    Tugas_Akhir

---
+++ src/tugas/akhir/TugasAkhir.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas.akhir;
+
+/**
+ *
+ * @author Angga
+ */
+public class TugasAkhir {
+    public static void main(String[] args){
+        new FrontApp().setVisible(true);
+    }
+}
